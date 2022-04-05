1d382f8e660a52ab7c07eb5df44d7a9189f3ce5a
Dhanang <dhanangap@outlook.com>
2022-04-05 20:41:43 +0700

    In browser environment, initialize all available components by default

--- src/bundle.ts
+++ src/bundle.ts
@@ -4,8 +4,11 @@ import Navbar from "./components/Navigation/Navbar/Navbar";
 import Scrollspy from "./components/Navigation/Scrollspy/Scrollspy";
 import Tab from "./components/Tab/Tab";
 
-Scrollspy.init();
-Form.FormComponent.init();
-Tab.init();
-Carousel.init();
-Navbar.init();
+// In browser environment, initialize all available components by default
+if (window) {
+	Scrollspy.init();
+	Form.FormComponent.init();
+	Tab.init();
+	Carousel.init();
+	Navbar.init();
+}
